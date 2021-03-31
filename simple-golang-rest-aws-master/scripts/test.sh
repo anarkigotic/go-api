869249d2267b925a901575ca41383f0c8e405e96
juan david jerez rubiano <JuanD.Jerez@Outsourcing.Libertycolombia.com>
2021-03-30 21:55:55 -0500

    first commit

---
+++ simple-golang-rest-aws-master/scripts/test.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+
+echo "Testing .go files"
+
+
+cd src/handlers/
+
+for folder in */;
+	do
+		if [ $folder == "vendor/" ] ; then
+			continue;
+		fi
+		
+		(cd $folder
+			for innerFile in *;
+				do
+					if [ $innerFile == *".out" ] ; then
+						rm $innerFile
+					fi
+
+					if [ $innerFile == *".html" ] ; then
+						rm $innerFile
+					fi
+
+					if [ $innerFile == *".pdf" ] ; then
+						rm $innerFile
+					fi
+				done
+      
+      go test -coverprofile=cover.out
+      go tool cover -html=cover.out -o cover.html
+      wkhtmltopdf cover.html cover.pdf
+    )
+	done
+
+echo "Done."
